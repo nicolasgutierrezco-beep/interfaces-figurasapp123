033127e30468af71403ff1e21acce107177b7bee
nicolasgutierrezco-beep <nicolas_gutierrezco@fet.edu.co>
2025-09-13 17:53:38 -0500

    Update Main.java

--- src/figuras/Main.java
+++ src/figuras/Main.java
@@ -1,4 +1,14 @@
 package figuras;
 
 public class Main {
+    public static void main(String[] args) {
+        FIgura f1 = new Cuadrado(4);
+        FIgura f2 = new Circulo(3);
+
+        System.out.println("Área cuadrado: " + f1.calcularArea());
+        System.out.println("Perímetro cuadrado: " + f1.calcularPerimetro());
+
+        System.out.println("Área círculo: " + f2.calcularArea());
+        System.out.println("Perímetro círculo: " + f2.calcularPerimetro());
+    }
 }
